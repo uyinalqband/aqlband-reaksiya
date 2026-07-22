cc929396735d32954f0b6423f46d15855b9ff54b
uyinalqband <uyin.aqlband@gmail.com>
2026-07-22 19:55:28 +0500

    Add files via upload

---
+++ src/types/progression.ts
@@ -0,0 +1,9 @@
+export interface ProgressionSnapshot {
+  totalXp: number;
+  level: number;
+  currentLevelXp: number;
+  nextLevelXp: number;
+  todayXp: number;
+  totalRewardedGames: number;
+  rank: number;
+}
