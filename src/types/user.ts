cb3880c49632d459452ac28672925ca3e0897338
uyinalqband <uyin.aqlband@gmail.com>
2026-07-22 19:00:52 +0500

    Delete src/types/user.ts

--- src/types/user.ts
+++
@@ -1,9 +0,0 @@
-import type { AppAccount } from '@/types/account';
-
-export type UserRow = AppAccount;
-
-export interface PublicUser {
-  id: string;
-  displayName: string;
-  username: string | null;
-}
