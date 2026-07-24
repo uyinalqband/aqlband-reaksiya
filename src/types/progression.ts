4db17290e5feec7941e20c72adb619a0d2a09ae1
uyinalqband <uyin.aqlband@gmail.com>
2026-07-24 10:22:01 +0500

    Delete src directory

--- src/types/progression.ts
+++
@@ -1,9 +0,0 @@
-export interface ProgressionSnapshot {
-  totalXp: number;
-  level: number;
-  currentLevelXp: number;
-  nextLevelXp: number;
-  todayXp: number;
-  totalRewardedGames: number;
-  rank: number;
-}
